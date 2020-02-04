97e043e754a68b9aeea9e4962cbeec1e9113f36d
marialuis-mp <marialmpatricio@hotmail.com>
2020-02-04 15:22:29 +0000

    Second topic of part 4

--- heat.hpp
+++ heat.hpp
@@ -58,6 +58,37 @@ public:
         }
     }
 
+    //Exact Solution
+    template<int dim>
+    void calc_exact(Vector<T>& x, Vector<T>& u, int& counter, T t){
+        for(x[dim]=1; x[dim]<=m; x[dim]++){
+            calc_exact(dim-1,x, counter);
+        }
+    }
+
+    //last iteration - 1 dimension, a line with m nodes
+    template<>
+    void calc_exact<1>(Vector<T>& x, Vector<T>& u, int& counter, T t){
+        double const dx=1/(m+1);
+        for(x[1]=1; x[1]<=m; x[1]++){
+            for (auto i = 0; i < n ; ++i){ //product of sin(pi*x_i) , 0<i<n-1
+                u[counter]*=sin(M_PI*x[i]*dx);
+                counter++;
+            }
+            u[counter]*=exp(-n*M_PI^2*alpha*t);
+            counter++;
+        }
+    }
+
+    template<typename U=T>
+    Vector<U> exact(U t) const{
+        Vector<U> u(pow(m,n)); //vector with the result
+        Vector<int> x(n); //vector that holds the current position
+        int counter=0;
+
+        calc_exact(x,u,counter,t);
+    }
+
 };
 
 #endif // HEAT_HPP
